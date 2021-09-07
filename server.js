87526ca3e327797a7207dac3f934cacabd4c02b6
MMORONTO <t2wycemo@gmail.com>
2021-09-07 16:40:00 -0400

    fetch data from server utilizing cors

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json());
-
 const database = {
 	users: [
 		{
@@ -32,6 +30,9 @@ const database = {
 		}
 	]
 }
+
+app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', (req, res)=> {
 	res.send(database.users);
