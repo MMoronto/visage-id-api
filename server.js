beb24c49b3e71e0eea66d96b30a75c2802a2b2c2
MMORONTO <t2wycemo@gmail.com>
2021-09-12 16:38:42 -0400

    Grab entries from database and increase the entry count

--- server.js
+++ server.js
@@ -90,11 +90,12 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-  db.where('id', '=', id)
-  .update({
-    status: 'archived',
-    thisKeyIsSkipped: undefined
-  })	
+  db.where('id', '=', id) 
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+  	console.log(entries);
+  })
 })
 
 app.listen(3000, ()=> {
