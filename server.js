9896a2f60a6fb1f959cf18c6f486472f9b299cc5
MMORONTO <t2wycemo@gmail.com>
2021-09-15 12:23:01 -0400

    separate profile function from server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 const db = knex({
   client: 'pg',
@@ -26,20 +27,11 @@ app.get('/', (req, res)=> {
 	res.send(database.users);
 })
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt ) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({id})
-		.then(user=> {
-			if(user.length) {
-			res.json(user[0])
-			} else {
-				res.status(400).json('not found')
-			}
-	})
+app.get('/profile/:id', (req, res) => {register.handleProfileGet(req, res, db)})
 		.catch(err => res.status(400).json('error getting user'))
 })
 
