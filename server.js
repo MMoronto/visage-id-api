81093b850f9b7e63b56e4e4461eb5567226f9e93
MMORONTO <t2wycemo@gmail.com>
2021-09-15 15:14:12 -0400

    add validation conditions to handle incorrect registration form submissions

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.get('/', (req, res)=> {
 	res.send(database.users);
 })
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, ()=> {
 	console.log('app is running on port 3000')
