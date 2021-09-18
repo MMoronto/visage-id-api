5919916b98992b6e0154b16e98f0b16d47c23bfe
MMORONTO <t2wycemo@gmail.com>
2021-09-17 23:43:29 -0400

    debug. fix api issue preventing signup data storage in db

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,7 @@
-const handleSignin = (req, res, db, bcrypt) => {
+const handleSignin = (db, bcrypt) => (req, res) => {
 	const { email, password } = req.body;
 	if (!email || !password) {
-		res.status(400).json('incorrect form submission');
+		return res.status(400).json('incorrect form submission');
 	}
 	db.select('email', 'hash').from('login')
 		.where('email', '=', email)
