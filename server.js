d999f88a3afc7cf0a0d95b87ccf9efee48bc47db
MMORONTO <t2wycemo@gmail.com>
2021-09-21 18:02:28 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-tapered-93467',
-    user : 't2wycemo',
-    password : '',
-    database : 'visage-id'
+    host : process.env.DATABASE_URL,
+    ssl : true,
   }
 });
 
