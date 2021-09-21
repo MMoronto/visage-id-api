cc6e117dcacccdeaa4600a5907b7e77da14cb2e5
MMORONTO <t2wycemo@gmail.com>
2021-09-21 17:46:31 -0400

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-tapered-93467',
     user : 't2wycemo',
     password : '',
     database : 'visage-id'
